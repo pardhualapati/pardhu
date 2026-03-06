67d9ef8652a6c9273464955270a282e9a2a0441c
pardhualapati <pardhualapati01@gmail.com>
2026-03-05 19:25:22 -0600

    updated app.js

--- app.js
+++ app.js
@@ -61,6 +61,8 @@ app.post("/contact", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
